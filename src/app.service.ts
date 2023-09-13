6ad0c03f440c13ab6b0d15a5b925d0350a5afb4a
harshitissm <goyalharshit120@gmail.com>
2023-09-13 19:08:19 +0530

    changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,29 +7,6 @@ export class AppService {
     return 'Hello World!';
   }
 
-  async getWeather() {
-    const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi&country=India';
-
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': '07c35b8f44msh20b400e144af541p1e3604jsn684455a67341',
-        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-      }
-    };
-
-    try {
-      const fetch = require('node-fetch');
-      const response = await fetch(url, options);
-      const result = await response.json();
-      console.log(result);
-      return result;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  }
-
   async getWeatherByCity(city: string) {
     city = city.replace(/ /g, "%20");
     const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city;
@@ -43,6 +20,7 @@ export class AppService {
     };
 
     try {
+      const fetch = require('node-fetch');
       const response = await fetch(url, options);
       const result = await response.json();
 
