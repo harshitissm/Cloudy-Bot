b0afcf699e595a59bf0305be9178599fbac6a838
harshitissm <goyalharshit120@gmail.com>
2023-09-14 14:32:04 +0530

    converting to axios

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,10 +9,12 @@ export class AppService {
 
   async getWeatherByCity(city: string) {
     city = city.replace(/ /g, "%20");
-    const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city;
+    const axios = require('axios');
 
     const options = {
       method: 'GET',
+      url: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+      params: { city: city },
       headers: {
         'X-RapidAPI-Key': '07c35b8f44msh20b400e144af541p1e3604jsn684455a67341',
         'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
@@ -20,15 +22,32 @@ export class AppService {
     };
 
     try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-
-      console.log(result);
-      return result;
+      const response = await axios.request(options);
+      console.log(response.data);
+      return response.data;
     } catch (error) {
       console.error(error);
-      throw error;
     }
+    // const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city;
+
+    // const options = {
+    //   method: 'GET',
+    //   headers: {
+    //     'X-RapidAPI-Key': '07c35b8f44msh20b400e144af541p1e3604jsn684455a67341',
+    //     'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    //   }
+    // };
+
+    // try {
+    //   const response = await fetch(url, options);
+    //   const result = await response.json();
+
+    //   console.log(result);
+    //   return result;
+    // } catch (error) {
+    //   console.error(error);
+    //   throw error;
+    // }
   }
 
 }
